0e5e3412792ad98e18b00f4070806a785102f76b
Adam Butler <adam@lab.io>
2015-03-01 22:34:22 +0000

    Generate lengthened url on Alias creation

--- app/models/alias.rb
+++ app/models/alias.rb
@@ -1,2 +1,16 @@
 class Alias < ActiveRecord::Base
+  before_create :generate_lengthened_url
+
+  private
+
+  def self.random_url
+    CONFIG["quotes"].sample(6)
+  end
+
+  def generate_lengthened_url
+    self.lengthened_url = loop do
+      lengthened_url = Alias.random_url.join("_")
+      break lengthened_url unless Alias.exists?(lengthened_url: lengthened_url)
+    end
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+CONFIG = YAML.load(File.read(File.join(File.dirname(__FILE__), 'spaaaccccce.yml')))
+
 module Spaaaccccce
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
