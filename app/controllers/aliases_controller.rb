b0f55e89e95bc1eb3ce664e4c491897e303fbfe4
Adam Butler <adam@lab.io>
2015-03-02 21:55:34 +0000

    + alias create action

--- app/controllers/aliases_controller.rb
+++ app/controllers/aliases_controller.rb
@@ -2,4 +2,17 @@ class AliasesController < ApplicationController
   def new
     @alias = Alias.new
   end
+
+  def create
+    @alias = Alias.where(original_url: params["alias"]["original_url"]).first_or_create
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  private
+
+  def alias_params
+    params.require(:alias).permit(:original_url)
+  end
 end
